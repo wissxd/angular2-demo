896eb6eb3a170237335ad970c61c046f2f1bb9aa
wissxd <wxdong91@gmail.com>
2017-05-12 12:06:56 +0800

    添加app.module.ts文件

---
+++ app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HighlightDirective } from './highlight.directive';
+import { LoggerService } from './logger.service';
+import { ChildComponent } from './child.component';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        FormsModule
+    ],
+    declarations: [AppComponent, HighlightDirective, ChildComponent],
+    providers: [LoggerService],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
