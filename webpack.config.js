000e1b2b107b8155a9fb482f082317b2bfd2ce4b
wissxd <wxdong91@gmail.com>
2017-05-12 11:53:14 +0800

    升级webpack2

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  entry: './main.ts',
+
+  output: {
+    filename: './bundle.js'
+  },
+
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader'
+      }
+    ]
+  }
+};
