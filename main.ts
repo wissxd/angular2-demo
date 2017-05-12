40940fef8ef7a4acd584db148697c47f68ca2944
wissxd <wxdong91@gmail.com>
2017-05-12 11:59:29 +0800

    添加main.ts文件

---
+++ main.ts
@@ -0,0 +1,10 @@
+import 'reflect-metadata';
+import 'zone.js';
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+ .then(success => console.log(`Bootstrap success`))
+ .catch(error => console.log(error))
